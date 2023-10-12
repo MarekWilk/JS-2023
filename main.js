a1b837d961f4b49acc685361bddccccc42376d2c
MarekWilk <maro_will@wp.pl>
2023-10-12 21:14:47 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,41 @@
-const liczba1 = document.querySelector('#liczba1')
-const btnPrzelicz = document.querySelector('#przelicz')
-const wynikiPojemnik = document.querySelector('#wyniki')
+   
+function suma(){
+    const l1 = parseFloat(document.querySelector("#liczba1").value) ||0
+    const l2 = parseFloat(document.querySelector("#liczba2").value) ||0
+    const l3 = parseFloat(document.querySelector("#liczba3").value) ||0
+    const l4 = parseFloat(document.querySelector("#liczba4").value) ||0
 
-btnPrzelicz.addEventListener('click', () => {
-    wynikiPojemnik.innerHTML = liczba1.value
-    console.log(liczba1.value)
-})
+    const sum = l1 + l2 + l3 +l4;
+    
+    document.querySelector("#suma").textContent = "Suma: "+ sum;
+}
+function srednia(){
+    const l1 = parseFloat(document.querySelector("#liczba1").value)
+    const l2 = parseFloat(document.querySelector("#liczba2").value)
+    const l3 = parseFloat(document.querySelector("#liczba3").value)
+    const l4 = parseFloat(document.querySelector("#liczba4").value)
+
+    const srednia = (l1 + l2 + l3 +l4)/4;
+
+    document.querySelector("#srednia").textContent = "Średnia: "+ srednia;
+}
+function min(){
+    const l1 = parseFloat(document.querySelector("#liczba1").value)
+    const l2 = parseFloat(document.querySelector("#liczba2").value)
+    const l3 = parseFloat(document.querySelector("#liczba3").value)
+    const l4 = parseFloat(document.querySelector("#liczba4").value)
+    
+    const min =  Math.min(l1,l2,l3,l4)
+
+    document.querySelector("#min").textContent = "Min: "+ min;
+}
+function max(){
+    const l1 = parseFloat(document.querySelector("#liczba1").value)
+    const l2 = parseFloat(document.querySelector("#liczba2").value)
+    const l3 = parseFloat(document.querySelector("#liczba3").value)
+    const l4 = parseFloat(document.querySelector("#liczba4").value)
+    
+    const max = Math.max(l1,l2,l3,l4)
+
+    document.querySelector("#max").textContent = "Max: "+ max;
+}
